34d31da07d0c59b9921c156ec0f8b0205c06e6b4
Standard Template <standardtemplateconstruct2022@gmail.com>
2023-01-19 19:19:13 +0200

    works

--- __main__.py
+++ __main__.py
@@ -49,27 +49,38 @@ LAUNCH_LIST = [
   TermParams(title="df", command="watch -n 2 df", geometry=TermGeometry(20, 8)),
 ]
 
+class Launcher:
+    processes_launched = []
 
-processes_launched = []
+    @classmethod
+    def kill(cls):
+        for p in cls.processes_launched:
+            print(f"@KILLING {p}")
+            p.kill()
 
-def kill_commands():
-    for p in processes_launched:
-        print(f"@KILLING {p}")
-        p.kill()
+    @classmethod
+    def is_launched(cls):
+        return len(cls.processes_launched) > 0
+    
+    @classmethod
+    def keep_alive(cls):
+        pass
+        #fixme implement
 
-def launch_commands():
-    kill_commands()
-    for cmd in LAUNCH_LIST:
-        print(f"@LAUCHING\n{cmd}\n")
-        if isinstance(cmd, TermParams):
-            exec_name = "/bin/urxvt"
-            argv = [cmd.title ] + shlex.split(str(cmd))
-        else:
-            exec_name = cmd.split(' ')[0]
-            argv = cmd.split(' ')[1:]
-        p = subprocess.Popen(argv, executable=exec_name)
-           #, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        processes_launched.append(p)
+    @classmethod
+    def launch(cls):
+        cls.kill()
+        for cmd in LAUNCH_LIST:
+            print(f"@LAUCHING\n{cmd}\n")
+            if isinstance(cmd, TermParams):
+                exec_name = "/bin/urxvt"
+                argv = [cmd.title ] + shlex.split(str(cmd))
+            else:
+                exec_name = cmd.split(' ')[0]
+                argv = cmd.split(' ')[1:]
+            p = subprocess.Popen(argv, executable=exec_name)
+            #, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            cls.processes_launched.append(p)
 
 
 
@@ -77,85 +88,95 @@ def launch_commands():
 
 
 app = Flask(__name__)
-SCENES = []
-SCENE_INDEX = 0
 
+
+class SceneSwitcher():
+    _obs_client = None
+    scenes = []
+    i = 0
+
+    @classmethod
+    def init_and_fetch_scenes(cls):
+        cls._obs_client = obs.ReqClient(host=OBS_HOST, port=OBS_PORT, password=OBS_PASSWORD)
+        resp = cls._obs_client.get_scene_list()
+        cls.scenes = [di.get("sceneName") for di in reversed(resp.scenes)]
+        print(f"######\n{SceneSwitcher.scenes}\n####")
+
+    @classmethod
+    def _update_scene(cls):
+        cls._obs_client.set_current_program_scene(cls.current())
+
+
+    @classmethod 
+    def next(cls):
+        if cls.i < len(cls.scenes)-1:
+            cls.i += 1
+        else:
+            cls.i = 0
+        cls._update_scene()
+    
+    @classmethod 
+    def prev(cls):
+        if cls.i > 0:
+            cls.i -= 1
+        else:
+            cls.i = len(cls.scenes)-1
+        cls._update_scene()
+
+
+    @classmethod
+    def switch(cls, name):
+        cls.i = cls.scenes.index(name)
+        cls._update_scene()
+
+
+    @classmethod
+    def current(cls):
+        return cls.scenes[cls.i]
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
-import asyncio
-import simpleobsws
+obs_client = None
 
-parameters = simpleobsws.IdentificationParameters() # Create an IdentificationParameters object (optional for connecting)
-
-ws = simpleobsws.WebSocketClient(url = f'ws://{OBS_HOST}:{OBS_PORT}') #, password = OBS_PASSWORD, identification_parameters = parameters) # Every possible argument has been passed, but none are required. See lib code for defaults.
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
-
-
-async def ws_connect():
-    await ws.connect() # Make the connection to obs-websocket
-    await ws.wait_until_identified() # Wait for the identification handshake to complete
-
-
-async def ws_disconnect():
-    await ws.disconnect()
-
-async def ws_get_scene_list():
-    request = simpleobsws.Request('GetVersion') # Build a Request object
-
-    ret = await ws.call(request) # Perform the request
-    if ret.ok(): # Check if the request succeeded
-        print("Request succeeded! Response data: {}".format(ret.responseData))
-    return ret
-
-async def fetch_scenes():
-    await ws_connect()
-    SCENES = await ws_get_scene_list()
-    print(SCENES)
-    await ws_disconnect()
-
+import obsws_python as obs
 
 def main():
-    # launch_commands()
-    #time.sleep(1000)
-    #loop.run_until_complete(ws_connect())
-    #SCENES = loop.run_until_complete(ws_get_scene_list())
     app.run(debug=True, port=8888)
-    
-    # obs_client = obs.ReqClient(host=OBS_HOST, port=OBS_PORT, password=OBS_PASSWORD)
-    # resp = obs_client.get_scene_list()
-    # SCENES = [di.get("sceneName") for di in reversed(resp.scenes)]
-    # for scene in scenes:
-    #     print(f"Switching to scene {scene}")
-    #     obs_client.set_current_program_scene(scene)
-    #     time.sleep(0.5)
+  
 
 
+@app.route("/launch", methods=["GET"])
+def launch():
+    Launcher.launch()
+    time.sleep(3)
+    SceneSwitcher.init_and_fetch_scenes()
+    return jsonify({"status": "launched"})
 
+@app.route("/kill", methods=["GET"])
+def kill():
+    if Launcher.is_launched():
+        Launcher.kill()
+        return jsonify({"status": "killed"})
+    else:
+        return jsonify({"status": "already_killed"})
 
 
 @app.route("/keepAlive", methods=["GET"])
 def keep_alive():
+    Launcher.keep_alive()
     return jsonify({"status": "alive"})
 
 @app.route("/scene/<scene_name>", methods=["GET"])
 def scene(scene_name=None):
-    if not SCENES:
-        loop.run_until_complete(fetch_scenes())
-    if scene_name in SCENES:
-        SCENE_INDEX = SCENES.index(scene_name)
+    if not Launcher.is_launched():
+        launch()
+    if scene_name in SceneSwitcher.scenes:
+        SceneSwitcher.switch(scene_name)
     elif scene_name == "next":
-        if SCENE_INDEX < len(SCENES):
-            SCENE_INDEX += 1
-        else:
-            SCENE_INDEX = 0
+        SceneSwitcher.next()
     elif scene_name == "prev":
-        if SCENE_INDEX > 0:
-            SCENE_INDEX -= 1
-        else:
-            SCENE_INDEX = len(SCENES)
-    new_scene = SCENES[SCENE_INDEX]
+        SceneSwitcher.prev()
+    new_scene = SceneSwitcher.current()
     return jsonify({"scene":new_scene})
 
 import signal
@@ -163,8 +184,8 @@ import atexit
 
 
 def handle_exit(*args):
-    loop.run_until_complete(ws_disconnect())
-    kill_commands()
+    #fixme close obs client
+    Launcher.kill()
     
 
 if __name__ == "__main__":
